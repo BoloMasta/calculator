4cde49cd8388be0d19abe92a4a55c57b5e9609eb
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-10-25 17:49:13 +0200

    js update
    add function operate() i displayNumber()

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const previousNumber = document.querySelector(".previousNumber");
-const mathSign = document.querySelector("mathSign");
-const currentNumber = document.querySelector("currentNumber");
+const mathSign = document.querySelector(".mathSign");
+const currentNumber = document.querySelector(".currentNumber");
 
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
@@ -12,13 +12,37 @@ const historyBtn = document.querySelector("history__button");
 
 let result = "";
 
+function displayNumber() {
+  if (this.textContent === "." && currentNumber.innerHTML.includes(".")) return;
+  if (this.textContent === "." && currentNumber.innerHTML === "")
+    return (currentNumber.innerHTML = "0.");
+  currentNumber.innerHTML += this.textContent;
+}
 
+function operate() {
+  if (this.textContent === "-" && currentNumber.innerHTML === "") {
+    currentNumber.innerHTML = "-";
+    return;
+  } else if (currentNumber.innerHTML === "") {
+    return;
+  }
+  if (mathSign !== "") {
+    showresult();
+  }
+  previousNumber.innerHTML = currentNumber.innerHTML;
+  mathSign.innerHTML = this.textContent;
+  currentNumber.innerHTML = "";
+}
 
+function showresult() {
+  if (previousNumber.innerHTML) {
+  }
+}
 
 operatorButtons.forEach((button) => button.addEventListener("click", operate));
 numberButtons.forEach((button) =>
   button.addEventListener("click", displayNumber)
 );
-equalBtn.addEventListener("click", showresult);
-clearBtn.addEventListener("click", clearScreen);
-historyBtn.addEventListener("click", clearHistory);
+// equalBtn.addEventListener("click", showresult);
+//clearBtn.addEventListener("click", clearScreen);
+//historyBtn.addEventListener("click", clearHistory);
